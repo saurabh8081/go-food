123b07941c3daef955f924ee19f4ee0c931e9808
saurabbh1120 <126014591+saurabh8081@users.noreply.github.com>
2023-08-12 17:42:16 +0530

    lec1

---
+++ src/screens/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+import Footer from '../components/Footer'
+
+const Home = () => {
+  return (
+    <div>
+      <div><Navbar/></div>
+      <div>Body</div>
+      <div><Footer/></div>
+    </div>
+  )
+}
+
+export default Home
